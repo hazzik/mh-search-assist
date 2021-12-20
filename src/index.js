b7b73ef923820389db89cd6b77b0ddc1d9239c69
Alex Zaytsev <hazzik@gmail.com>
2021-12-20 13:06:58 +1300

    Improve suppor for Family search links

--- src/index.js
+++ src/index.js
@@ -13,53 +13,37 @@ function debounce(func, wait, immediate) {
 }
 
 function processLinks () {
-    document.querySelectorAll('.searchResultsRecord').forEach(d => {
-        const name = d.querySelector('a.recordTitle').textContent.trim().replace(/\W+/gu,'-').toLowerCase();
-        const params = new URLSearchParams(d.getAttribute('data-callback-extra-parameter'));
+    document.querySelectorAll('.recordContainer').forEach(container => {
+        const link = container.querySelector('a.recordTitle');
+        const name = link.textContent.trim();
+        const params = getParameters(container, link);
+        if (!params) return;
         const colId = params.get('colId');
         const itemId = params.get('itemId');
-        const url = createUrl(colId, itemId, name);
-        if (url) {
-            d.querySelectorAll('a.record_link').forEach(a => {
-                a.classList.add('unlocked');
-                a.setAttribute('href', url);
-            });
-        }
-    });
-    
-    document.querySelectorAll('.result_list_row').forEach(d => {
-        const link = d.querySelector('.record_title a.record_name');
-        const name = link.textContent.trim().replace(/\W+/gu,'-').toLowerCase();
-        const [,colId, itemId] = /record-(\d+)-([^//]+)/.exec(link.getAttribute('href')) ?? [];
-        const url = createUrl(colId, itemId, name);
-        if (url) {
-            d.querySelectorAll('a.record_name').forEach(a => {
-                a.style.setProperty('border', '1px solid black');
-                a.setAttribute('href', url);
-                //a.setAttribute('target', '_blank');
-            });
-        }
+        const url = createUrl(colId, itemId, name, container);
+        if (!url) return;
+        container.querySelectorAll('a.record_link').forEach(a => {
+            a.classList.add('unlocked');
+            a.setAttribute('href', url);
+        });
     });
 
-    document.querySelectorAll('.records_list_item').forEach(d => {
-        const link = d.querySelector('.record_title a.record_name');
-        const name = link.textContent.trim().replace(/\W+/gu,'-').toLowerCase();
-        const href = link.getAttribute('href');
-        if (/search-plans.php\?/.test(href)) {
-            const params = new URL(href).searchParams;
-            const colId = params.get('colId');
-            const itemId = params.get('itemId');
-            const url = createUrl(colId, itemId, name);
-            if (url) {
-                d.querySelectorAll('a.record_name').forEach(a => {
-                    a.classList.add('unlocked');
-                    a.setAttribute('href', url);
-                });
-                d.onclick = (e) => {
-                    e.stopPropagation();
-                };
-            }
-        }
+    document.querySelectorAll('.records_list_item').forEach(container => {
+        const link = container.querySelector('.record_title a.record_name');
+        const name = link.textContent.trim();
+        const params = getParameters(container, link);
+        if (!params) return;
+        const colId = params.get('colId');
+        const itemId = params.get('itemId');
+        const url = createUrl(colId, itemId, name, container);
+        if (!url) return;
+        container.querySelectorAll('a.record_name').forEach(a => {
+            a.classList.add('unlocked');
+            a.setAttribute('href', url);
+        });
+        container.onclick = (e) => {
+            e.stopPropagation();
+        };
     });
 
     document.querySelectorAll('.record_match_action_container .record_match_action_icon.record_match_unconfirmed').forEach(a => {
@@ -70,21 +54,54 @@ function processLinks () {
     });
 };
 
-function createUrl(colId, itemId, name) {
+function getParameters(container, link) {
+    const cbExtraParameter = container.getAttribute('data-callback-extra-parameter');
+    if (cbExtraParameter) {
+        return new URLSearchParams(cbExtraParameter);
+    }
+    
+    const href = link.href;
+    if (/\/search-plans.php\?/.test(href)) {
+        return new URL(href).searchParams;
+    }
+
+    return null;
+}
+
+function extractFromTable(container, field) {
+    return document.evaluate(`.//tr[contains(td, "${field}:")]/td[2]|.//li[contains(span, "${field}")]/span[2]`, container).iterateNext()?.textContent;
+}
+
+function createUrl(colId, itemId, name, container) {
     switch (colId) {
         case '1': {
             const [_, siteId, familyTreeId, individualId] = itemId.match(/(\d+)-(\d+)-(\d+)/);
-            return `https://www.myheritage.com/person-${(parseInt(familyTreeId) * 1000000 + parseInt(individualId))}_${siteId}_${siteId}/${name}`;
+            const nameSlug = name.replace(/\W+/gu,'-').toLowerCase();
+            return `https://www.myheritage.com/person-${(parseInt(familyTreeId) * 1000000 + parseInt(individualId))}_${siteId}_${siteId}/${nameSlug}`;
         }
         case '3': {
-            return `https://www.myheritage.com/member-${itemId}_1/${name}`;
+            const nameSlug = name.replace(/\W+/gu,'-').toLowerCase();
+            return `https://www.myheritage.com/member-${itemId}_1/${nameSlug}`;
         }
         case '40001': {
             // Family Search
-            const ix = name.lastIndexOf(' ');
-            const firstName = name.substring(0, ix);
-            const lastName = name.substring(ix + 1);
-            return `https://www.familysearch.org/tree/find/name?self=${firstName}|${lastName}|1|1`;
+            const [_, fullName, birthName] = /\s*([\w\s]+)(?:\(\w+ (\w+)\))?\s*/.exec(name);
+            const ix = fullName.trim().lastIndexOf(' ');
+            const firstName = fullName.substring(0, ix);
+            const lastName = birthName || fullName.substring(ix + 1);
+            const params = new URLSearchParams(`self=${firstName}|${lastName}|1|1`);
+            const birth = extractFromTable(container, 'Birth');
+            if (birth) {
+                const [_, year, place ] = /(?:\w+\s+)?(\d+)(?:\s+-\s+(.*))?/.exec(birth);
+                params.append('birth', `${ place && place !== 'Place' ? place : '' }|${year || ''}|0|1`);
+            }
+            const death = extractFromTable(container, 'Death');
+            if (death) {
+                const [_, year, place ] = /(?:\w+\s+)?(\d+)(?:\s+-\s+(.*))?/.exec(death);
+                params.append('death', `${ place && place !== 'Place' ? place : '' }|${year || ''}|0|1`);
+            }
+
+            return `https://www.familysearch.org/tree/find/name?${params}`;
         }
     }
     return null;
