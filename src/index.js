0e0ca1130bf4e34004de63ba0630d52e2dad4058
Alex Zaytsev <hazzik@gmail.com>
2021-12-20 17:46:36 +1300

    Use external debounce

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		clearTimeout(timeout);
-		timeout = setTimeout(function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		}, wait);
-		if (immediate && !timeout) func.apply(context, args);
-	};
-}
+import debounce from "debounce";
 
 function processLinks () {
     document.querySelectorAll('.recordContainer').forEach(container => {
