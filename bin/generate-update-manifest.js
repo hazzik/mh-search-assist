36dac749009144c790f6c2c274437710d9937a9e
Alex Zaytsev <hazzik@gmail.com>
2021-12-22 10:39:36 +1300

    Use Github releases for distribution

--- bin/generate-update-manifest.js
+++ bin/generate-update-manifest.js
@@ -1,16 +1,15 @@
-import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { argv } from 'process';
 const { version } = JSON.parse(readFileSync('./package.json'));
-const file = readdirSync('./web-ext-artifacts/').pop();
 
 const manifest = JSON.stringify({ 
     addons: {
         [argv[2]]: {
             updates: [{
                 version,
-                update_link: `https://hazzik.github.io/mh-search-assist/web-ext-artifacts/${file}`
+                update_link: `https://github.com/hazzik/mh-search-assist/releases/latest/download/mh-search-assist.xpi`
             }]
         }
     }
 });
-writeFileSync('./update.json.tmp', manifest);
+writeFileSync('./web-ext-artifacts/update.json', manifest);
