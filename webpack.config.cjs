c69e72744d4bf5b739c5f481105d967c534e258b
Alex Zaytsev <hazzik@gmail.com>
2022-01-03 15:17:27 +1300

    Embed images into CSS to fix Chrome issues

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,29 +1,47 @@
-const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const package = require('./package.json');
 
 module.exports = {
 	devtool: 'source-map',
-	mode: 'production',	
+	mode: 'production',
+	entry: {
+		main: [
+			'./src/index.js',
+			'./src/index.css'
+		]
+	},
 	plugins: [
+		new MiniCssExtractPlugin(),
 		new CopyPlugin({
 			patterns: [
-            	{ 
+				{
 					from: 'src/manifest.json',
 					transform(content) {
 						const manifest = JSON.parse(content.toString());
 						manifest.version = package.version;
 						return JSON.stringify(manifest);
 					}
-				},
-				{
-					from: "**/*",
-          			context: path.resolve(__dirname, "src"),
-					globOptions: {
-						ignore: [ "**/*.js" ]
-					}
 				}
-    	    ]
+			]
 		})
-	]
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: 'asset/inline',
+			},
+		],
+	},
+	optimization: {
+		minimizer: [
+			new CssMinimizerPlugin(),
+		],
+	},
 };
